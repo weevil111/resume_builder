98a53a6c0dfcef1dc383b91cc7d5d7a2b2b678ff
weevil <abhinavparagmishra23@gmail.com>
2021-08-30 10:04:02 +0530

    Initial starter code

---
+++ src/config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+// Initialize Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyDRG6mdOiFTd69-0MWzjygzfROGuLqd7eY",
+    authDomain: "resume-builder-5dfd3.firebaseapp.com",
+    projectId: "resume-builder-5dfd3",
+    storageBucket: "resume-builder-5dfd3.appspot.com",
+    messagingSenderId: "228843477597",
+    appId: "1:228843477597:web:b4bbfb82d7441e09a18bb4"
+});
+export default firebase;
