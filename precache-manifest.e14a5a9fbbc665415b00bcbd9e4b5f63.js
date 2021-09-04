70d582adfb3cacd2e4c90c9b7779acbe024cc586
weevil <abhinavparagmishra23@gmail.com>
2021-09-04 15:19:17 +0530

    Updates

---
+++ precache-manifest.e14a5a9fbbc665415b00bcbd9e4b5f63.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526b87738b643c5c1116eca7b4062723",
+    "url": "/resume_builder/index.html"
+  },
+  {
+    "revision": "75e0210cd1afac436f55",
+    "url": "/resume_builder/static/css/main.8106c5dc.chunk.css"
+  },
+  {
+    "revision": "e30f9aacf8a74862186e",
+    "url": "/resume_builder/static/js/2.91823493.chunk.js"
+  },
+  {
+    "revision": "c107334464d556b24254c8926edb8edc",
+    "url": "/resume_builder/static/js/2.91823493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a14ab378bedcaaef8c",
+    "url": "/resume_builder/static/js/3.61f52ccb.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/resume_builder/static/js/3.61f52ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a5e317f6ea7db17119",
+    "url": "/resume_builder/static/js/4.5fe9a9d9.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/resume_builder/static/js/4.5fe9a9d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75e0210cd1afac436f55",
+    "url": "/resume_builder/static/js/main.e7991224.chunk.js"
+  },
+  {
+    "revision": "29df1a4381504eac0360",
+    "url": "/resume_builder/static/js/runtime-main.2c099f02.js"
+  },
+  {
+    "revision": "03aaebf151af139bd05df53dc5b362c3",
+    "url": "/resume_builder/static/media/aboutus.03aaebf1.jpg"
+  },
+  {
+    "revision": "011420942844a817d8ee7066ff1879e8",
+    "url": "/resume_builder/static/media/logo.01142094.png"
+  },
+  {
+    "revision": "7e7f9ba4dad8b0374b038b5c4ba13ebb",
+    "url": "/resume_builder/static/media/resume.7e7f9ba4.png"
+  }
+]);
